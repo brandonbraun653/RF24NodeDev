14a49c80e29c7059e5f0d0f9b8e3e737dcd23d9c
Brandon Braun <brandonbraun653@gmail.com>
2020-01-07 16:49:18 -0600

    Submodule update.

--- dev/VisualStudio/Simulator/EmbeddedDLL/EmbeddedDLL/exported_test_functions.cpp
+++ dev/VisualStudio/Simulator/EmbeddedDLL/EmbeddedDLL/exported_test_functions.cpp
@@ -1,3 +1,15 @@
+/********************************************************************************
+*  File Name:
+*    exported_test_functions.cpp
+*
+*  Description:
+*    
+*
+*  2020 | Brandon Braun | brandonbraun653@gmail.com
+********************************************************************************/
+
+/* C++ Includes */
+#include <thread>
 
 /* Boost Includes */
 #include <boost/asio.hpp>
@@ -5,6 +17,9 @@
 
 /* Project Includes */
 #include "exported_test_functions.hpp"
+
+/* Chimera Includes */
+#include <Chimera/chimera.hpp>
 
 /* RF24 Includes */
 #include <RF24Node/common/conversion.hpp>
@@ -19,36 +34,48 @@
 #include <uLog/ulog.hpp>
 #include <uLog/sinks/sink_cout.hpp>
 
-static boost::asio::io_service ioService;
-
+static void MasterNodeThread();
+static void SlaveNodeThread();
 
 void test_PipeCommunication()
 {
-  using namespace RF24::Network;
+  ChimeraInit();
 
+  uLog::initialize();
+
+  uLog::SinkHandle rootSink = std::make_shared<uLog::CoutSink>();
+  rootSink->setLogLevel( uLog::Level::LVL_DEBUG );
+  rootSink->setName( "Root" );
+  rootSink->enable();
+  
+  uLog::registerSink( rootSink );
+  uLog::setGlobalLogLevel( uLog::Level::LVL_DEBUG );
+  uLog::setRootSink( rootSink );
+
+  /*------------------------------------------------
+  Create master/slave hardware threads
+  ------------------------------------------------*/
+  std::thread masterThread = std::thread( MasterNodeThread );
+  std::thread slaveThread = std::thread( SlaveNodeThread );
+
+  while ( true )
+  {
+    boost::this_thread::sleep_for( boost::chrono::milliseconds( 100 ) );
+  }
+
+}
+
+void MasterNodeThread()
+{
   uLog::SinkHandle masterSink = std::make_shared<uLog::CoutSink>();
   masterSink->setLogLevel( uLog::Level::LVL_DEBUG );
   masterSink->setName( "Master" );
-
-  uLog::SinkHandle slaveSink = std::make_shared<uLog::CoutSink>();
-  slaveSink->setLogLevel( uLog::Level::LVL_DEBUG );
-  slaveSink->setName( "Slave" );
-
-  uLog::initialize();
-  uLog::registerSink( masterSink );
-  uLog::registerSink( slaveSink );
-
   masterSink->enable();
-  slaveSink->enable();
-  uLog::setGlobalLogLevel( uLog::Level::LVL_DEBUG );
+  uLog::registerSink( masterSink );
 
-  /*------------------------------------------------
-  Create a master node
-  ------------------------------------------------*/
   RF24::Endpoint master;
-
   RF24::EndpointConfig cfg;
-  cfg.network.mode = Mode::NET_MODE_STATIC;
+  cfg.network.mode = RF24::Network::Mode::NET_MODE_STATIC;
   cfg.network.nodeStaticAddress = RF24::RootNode0;
   cfg.network.parentStaticAddress = RF24::Network::RSVD_ADDR_INVALID;
   cfg.network.rxQueueBuffer = nullptr;
@@ -63,30 +90,47 @@ void test_PipeCommunication()
   master.attachLogger( masterSink );
   master.configure( cfg );
 
-  /*------------------------------------------------
-  Create a slave node
-  ------------------------------------------------*/
-  RF24::Endpoint slave;
 
+  while ( true )
+  {
+    master.processNetworking();
+
+    Chimera::delayMilliseconds( 25 );
+  }
+}
+
+void SlaveNodeThread()
+{
+  Chimera::delayMilliseconds( 150 );
+
+  uLog::SinkHandle slaveSink = std::make_shared<uLog::CoutSink>();
+  slaveSink->setLogLevel( uLog::Level::LVL_DEBUG );
+  slaveSink->setName( "Slave" );
+  slaveSink->enable();
+  uLog::registerSink( slaveSink );
+
+  RF24::Endpoint slave;
+  RF24::EndpointConfig cfg;
+  cfg.network.mode = RF24::Network::Mode::NET_MODE_STATIC;
   cfg.network.nodeStaticAddress = 0001;
   cfg.network.parentStaticAddress = RF24::RootNode0;
+  cfg.network.rxQueueBuffer = nullptr;
+  cfg.network.rxQueueSize = 5 * RF24::Hardware::PACKET_WIDTH;
+  cfg.network.txQueueBuffer = nullptr;
+  cfg.network.txQueueSize = 5 * RF24::Hardware::PACKET_WIDTH;
+
+  cfg.physical.dataRate = RF24::Hardware::DataRate::DR_1MBPS;
+  cfg.physical.powerAmplitude = RF24::Hardware::PowerAmplitude::PA_HIGH;
+  cfg.physical.rfChannel = 96;
+
 
   slave.attachLogger( slaveSink );
   slave.configure( cfg );
   slave.connect( 1000 );
 
-
-  while (!slave.isConnected())
-  {
-    masterSink->flog(uLog::Level::LVL_INFO, "Still waiting for slave to connect...\n" );
-    boost::this_thread::sleep_for( boost::chrono::milliseconds( 1000 ) );
-  }
-
   while ( true )
   {
-   
 
-    boost::this_thread::sleep_for( boost::chrono::milliseconds( 100 ) );
+    Chimera::delayMilliseconds( 25 );
   }
-
 }
